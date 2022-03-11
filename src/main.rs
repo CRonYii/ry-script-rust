dc7c624be735ecd492c6dd3bf736e5f0485236d6
Rongyi Chen <kenchenrong@gmail.com>
2022-03-10 20:20:42 -0800

    rewrite mul in a straight forward way

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,6 @@ fn main() {
     let m1 = matrix![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]];
     let m2 = matrix![[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]];
     let m3 = m1 * m2;
-    // let m3 = match m1.mul(&m2) {
-    //     Ok(m) => m,
-    //     Err(e) => panic!("MatrixError: {}", e),
-    // };
     println!("m3 = {:#?}", &m3);
 }
 
@@ -35,7 +31,7 @@ impl Matrix {
     }
 
     fn from(eles: Array2<f64>) -> Matrix {
-        let dim = eles.raw_dim();
+        let dim = eles.shape();
         let shape = (dim[0], dim[1]);
         Matrix {
             shape,
@@ -50,7 +46,6 @@ impl Matrix {
     fn n_cols(&self) -> usize {
         self.shape.1
     }
-
 }
 
 impl ops::Mul<Matrix> for Matrix {
@@ -63,30 +58,20 @@ impl ops::Mul<Matrix> for Matrix {
                 self.shape, other.shape
             )
         }
-        let data = self
-            .container
-            .axis_iter(Axis(0))
-            .map(|row| {
-                other
-                    .container
-                    .axis_iter(Axis(1))
-                    .map(|col| {
-                        match row
-                            .iter()
-                            .zip(col.iter())
-                            .map(|(a, b)| (*a) * (*b))
-                            .reduce(|accum, i| accum + i)
-                        {
-                            Some(r) => r,
-                            None => 0.0,
-                        }
-                    })
-                    .collect::<Vec<_>>()
-            })
-            .flatten()
-            .collect();
-        let container =
-            Array2::<f64>::from_shape_vec((self.n_rows(), other.n_cols()), data).unwrap();
+        let n_rows = self.n_rows();
+        let n_cols = other.n_cols();
+        let mut container = Array2::<f64>::zeros((n_rows, n_cols));
+        for r in 0..n_rows {
+            for c in 0..n_cols {
+                let row = self.container.row(r);
+                let col = other.container.column(c);
+                let mut sum = 0.0;
+                for x in 0..self.n_cols() {
+                    sum += row[[x]] * col[[x]];
+                }
+                container[[r, c]] = sum;
+            }
+        }
         Matrix::from(container)
     }
 }
