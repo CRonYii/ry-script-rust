0d13c484c431c5330f081ef7c17bca5601cd22d3
Rongyi Chen <kenchenrong@gmail.com>
2022-03-10 18:39:53 -0800

    operator overloading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ndarray::prelude::*;
 use std::fmt::Debug;
+use std::ops;
 
 #[macro_export]
 macro_rules! matrix {
@@ -11,10 +12,11 @@ macro_rules! matrix {
 fn main() {
     let m1 = matrix![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]];
     let m2 = matrix![[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]];
-    let m3 = match m1.mul(&m2) {
-        Ok(m) => m,
-        Err(e) => panic!("MatrixError: {}", e),
-    };
+    let m3 = m1 * m2;
+    // let m3 = match m1.mul(&m2) {
+    //     Ok(m) => m,
+    //     Err(e) => panic!("MatrixError: {}", e),
+    // };
     println!("m3 = {:#?}", &m3);
 }
 
@@ -49,38 +51,42 @@ impl Matrix {
         self.shape.1
     }
 
-    fn mul(&self, other: &Matrix) -> Result<Matrix, String> {
+}
+
+impl ops::Mul<Matrix> for Matrix {
+    type Output = Matrix;
+
+    fn mul(self, other: Matrix) -> Matrix {
         if self.n_cols() != other.n_rows() {
-            Err(format!(
-                "Shape {:?} and {:?} not aligned",
+            panic!(
+                "Matrix multiplication error: shape {:?} and {:?} not aligned",
                 self.shape, other.shape
-            ))
-        } else {
-            let data = self
-                .container
-                .axis_iter(Axis(0))
-                .map(|row| {
-                    other
-                        .container
-                        .axis_iter(Axis(1))
-                        .map(|col| {
-                            match row
-                                .iter()
-                                .zip(col.iter())
-                                .map(|(a, b)| (*a) * (*b))
-                                .reduce(|accum, i| accum + i)
-                            {
-                                Some(r) => r,
-                                None => 0.0,
-                            }
-                        })
-                        .collect::<Vec<_>>()
-                })
-                .flatten()
-                .collect();
-            let container =
-                Array2::<f64>::from_shape_vec((self.n_rows(), other.n_cols()), data).unwrap();
-            Ok(Matrix::from(container))
+            )
         }
+        let data = self
+            .container
+            .axis_iter(Axis(0))
+            .map(|row| {
+                other
+                    .container
+                    .axis_iter(Axis(1))
+                    .map(|col| {
+                        match row
+                            .iter()
+                            .zip(col.iter())
+                            .map(|(a, b)| (*a) * (*b))
+                            .reduce(|accum, i| accum + i)
+                        {
+                            Some(r) => r,
+                            None => 0.0,
+                        }
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .flatten()
+            .collect();
+        let container =
+            Array2::<f64>::from_shape_vec((self.n_rows(), other.n_cols()), data).unwrap();
+        Matrix::from(container)
     }
 }
