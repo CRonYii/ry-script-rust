6e8a7c0861aa9cced5f766af77422bd1d7bb0942
Rongyi Chen <kenchenrong@gmail.com>
2022-03-10 18:29:17 -0800

    force matrix to use f64

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use ndarray::prelude::*;
-use std::clone::Clone;
 use std::fmt::Debug;
 
-use num_traits::Num;
-
 fn main() {
-    let m1 = Matrix::from(array![[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
-    let m2 = Matrix::from(array![[1, 2], [3, 4], [5, 6]]);
+    let m1 = Matrix::from(array![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]);
+    let m2 = Matrix::from(array![[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]]);
     let m3 = match m1.mul(&m2) {
         Ok(m) => m,
         Err(e) => panic!("MatrixError: {}", e),
@@ -15,20 +12,20 @@ fn main() {
 }
 
 #[derive(Debug)]
-struct Matrix<T> {
+struct Matrix {
     shape: (usize, usize),
-    container: Array2<T>,
+    container: Array2<f64>,
 }
 
-impl<T: Num + Copy + Clone + Debug> Matrix<T> {
-    fn new(shape: (usize, usize)) -> Matrix<T> {
+impl Matrix {
+    fn new(shape: (usize, usize)) -> Matrix {
         Matrix {
             shape,
             container: Array2::zeros(shape),
         }
     }
 
-    fn from(eles: Array2<T>) -> Matrix<T> {
+    fn from(eles: Array2<f64>) -> Matrix {
         let dim = eles.raw_dim();
         let shape = (dim[0], dim[1]);
         Matrix {
@@ -45,7 +42,7 @@ impl<T: Num + Copy + Clone + Debug> Matrix<T> {
         self.shape.1
     }
 
-    fn mul(&self, other: &Matrix<T>) -> Result<Matrix<T>, String> {
+    fn mul(&self, other: &Matrix) -> Result<Matrix, String> {
         if self.n_cols() != other.n_rows() {
             Err(format!(
                 "Shape {:?} and {:?} not aligned",
@@ -67,7 +64,7 @@ impl<T: Num + Copy + Clone + Debug> Matrix<T> {
                                 .reduce(|accum, i| accum + i)
                             {
                                 Some(r) => r,
-                                None => T::zero(),
+                                None => 0.0,
                             }
                         })
                         .collect::<Vec<_>>()
@@ -75,8 +72,8 @@ impl<T: Num + Copy + Clone + Debug> Matrix<T> {
                 .flatten()
                 .collect();
             let container =
-                Array2::<T>::from_shape_vec((self.n_rows(), other.n_cols()), data).unwrap();
-            Ok(Matrix::<T>::from(container))
+                Array2::<f64>::from_shape_vec((self.n_rows(), other.n_cols()), data).unwrap();
+            Ok(Matrix::from(container))
         }
     }
 }
