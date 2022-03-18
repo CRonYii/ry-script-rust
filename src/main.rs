02d5a63effd8eeb51b5b2d01507db8e0073f4515
Rongyi Chen <kenchenrong@gmail.com>
2022-03-18 14:55:55 -0700

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::*;
+
 mod math;
 mod script;
 
@@ -8,13 +10,24 @@ pub use crate::math::matrix::*;
 pub use crate::script::lexer::lexer_parse;
 
 fn main() {
-    let tokens = lexer_parse(
-        "
-        # simple variable declartion
-        foo = 1;
-        bar = \"hello world\";
-        println(bar * 3);
-        ",
-    );
-    println!("parsed = {:?}", tokens);
+    let mut line = String::new();
+    loop {
+        print!("> ");
+        match stdout().flush() {
+            Ok(_) => (),
+            Err(error) => println!("{}", error),
+        }
+        let _ = stdin().read_line(&mut line).unwrap();
+        if line.starts_with(".exit") {
+            break;
+        }
+        match lexer_parse(&line) {
+            Ok(tokens) => {
+                dbg!(tokens);
+                ()
+            }
+            Err(err) => eprintln!("{}", err),
+        }
+        line.clear();
+    }
 }
